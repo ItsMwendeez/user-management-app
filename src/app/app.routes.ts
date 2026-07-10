ef847723b790571dcad1d301e64a13b67acc01d7
Patience Genesis Mwende Njagi <KEN219540@kcbgroup.com>
2026-07-10 11:40:25 +0300

    Fully designed Register user page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { RegisterUser } from './register-user/register-user';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'register',
+    component: RegisterUser
   }
 ];
